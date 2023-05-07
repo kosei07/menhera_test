44af7a6bfea94fe70dbf59dcd851fa52b17a711f
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-07 22:10:43 +0900

    update: router for profile/update

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,54 +1,123 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { Header } from './components/header';
 import SignUp from './features/auth/sign_up/index';
 import SignIn from './features/auth/sign_in/index';
 import BookCreate from './features/book/create/index';
 import BookList from './features/book/list/index';
 import BookDetail from './features/book/detail/index';
 import BookUpdate from './features/book/update/index';
-import Profile from './features/profile/create/index';
+import ProfileCreate from './features/profile/create/index';
+import ProfileUpdate from './features/profile/update/index';
 import { AuthGuard } from './utils/auth_guard';
 
 export const Router = createBrowserRouter([
-  { path: '/auth/sign_up', element: <SignUp /> },
-  { path: '/auth/sign_in', element: <SignIn /> },
-  {
-    path: '/profile/create',
-    element: (
-      <AuthGuard>
-        <Profile />
-      </AuthGuard>
-    ),
-  },
-  {
-    path: '/book/create',
-    element: (
-      <AuthGuard>
-        <BookCreate />
-      </AuthGuard>
-    ),
-  },
-  {
-    path: '/book/detail',
-    element: (
-      <AuthGuard>
-        <BookDetail />
-      </AuthGuard>
-    ),
-  },
-  {
-    path: '/book/update',
-    element: (
-      <AuthGuard>
-        <BookUpdate />
-      </AuthGuard>
-    ),
-  },
   {
     path: '/',
-    element: (
-      <AuthGuard>
-        <BookList />
-      </AuthGuard>
-    ),
+    element: <Header />,
+    children: [
+      { path: '/auth/sign_up', element: <SignUp /> },
+      { path: '/auth/sign_in', element: <SignIn /> },
+      {
+        path: '/profile/create',
+        element: (
+          <AuthGuard>
+            <ProfileCreate />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: '/profile/update',
+        element: (
+          <AuthGuard>
+            <ProfileUpdate />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: '/book/create',
+        element: (
+          <AuthGuard>
+            <BookCreate />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: '/book/detail',
+        element: (
+          <AuthGuard>
+            <BookDetail />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: '/book/update',
+        element: (
+          <AuthGuard>
+            <BookUpdate />
+          </AuthGuard>
+        ),
+      },
+      {
+        index: true,
+        element: (
+          <AuthGuard>
+            <BookList />
+          </AuthGuard>
+        ),
+      },
+    ],
   },
 ]);
+// export const Router = createBrowserRouter([
+//   { path: '/', element: <Header /> },
+//   { path: '/auth/sign_up', element: <SignUp /> },
+//   { path: '/auth/sign_in', element: <SignIn /> },
+//   {
+//     path: '/profile/create',
+//     element: (
+//       <AuthGuard>
+//         <ProfileCreate />
+//       </AuthGuard>
+//     ),
+//   },
+//   {
+//     path: '/profile/create',
+//     element: (
+//       <AuthGuard>
+//         <ProfileUpdate />
+//       </AuthGuard>
+//     ),
+//   },
+//   {
+//     path: '/book/create',
+//     element: (
+//       <AuthGuard>
+//         <BookCreate />
+//       </AuthGuard>
+//     ),
+//   },
+//   {
+//     path: '/book/detail',
+//     element: (
+//       <AuthGuard>
+//         <BookDetail />
+//       </AuthGuard>
+//     ),
+//   },
+//   {
+//     path: '/book/update',
+//     element: (
+//       <AuthGuard>
+//         <BookUpdate />
+//       </AuthGuard>
+//     ),
+//   },
+//   {
+//     index: true,
+//     element: (
+//       <AuthGuard>
+//         <BookList />
+//       </AuthGuard>
+//     ),
+//   },
+// ]);

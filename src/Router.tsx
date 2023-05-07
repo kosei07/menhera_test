3f598a4a3d77a524ffc54cb6398ae3da04453581
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-07 16:14:42 +0900

    update: routing path

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,11 +4,14 @@ import SignIn from './features/auth/sign_in/index';
 import BookCreate from './features/book/create/index';
 import BookList from './features/book/list/index';
 import BookDetail from './features/book/detail/index';
+import Profile from './features/profile/index'
+import { AuthGuard } from './utils/auth_guard';
 
 export const Router = createBrowserRouter([
   { path: '/auth/sign_up', element: <SignUp /> },
   { path: '/auth/sign_in', element: <SignIn /> },
-  { path: '/book/create', element: <BookCreate /> },
-  { path: '/', element: <BookList /> },
-  { path: '/book/detail', element: <BookDetail /> },
+  { path: '/profile/create', element: <AuthGuard><Profile /></AuthGuard> },
+  { path: '/book/create', element: <AuthGuard><BookCreate /></AuthGuard> },
+  { path: '/', element: <AuthGuard><BookList /></AuthGuard> },
+  { path: '/book/detail', element: <AuthGuard><BookDetail /></AuthGuard> },
 ]);

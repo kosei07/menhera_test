56e368448c685ceb5596adcf7ff68cf8a86709af
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-07 22:30:12 +0900

    update: header for profile/update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,47 @@
-import { type FC } from 'react'
-import { UserContext } from '../../contexts/user'
-import { useContext } from 'react'
-import {auth ,signOut,FirebaseError} from "../../utils/firebase"
-export const Header:FC = () => {
-  const userContext = useContext(UserContext)
-  const handleSignOut = ():void => {
+import { type FC } from 'react';
+import { UserContext } from '../../contexts/user';
+import { useContext } from 'react';
+import { auth, signOut, FirebaseError } from '../../utils/firebase';
+import { useNavigate, Outlet } from 'react-router-dom';
+
+export const Header: FC = () => {
+  const navigate = useNavigate();
+  const userContext = useContext(UserContext);
+  const handleSignOut = (): void => {
     try {
-      signOut(auth).then(()=>{console.log('サインアウト')}).catch(e=>{console.log(e)})
+      signOut(auth)
+        .then(() => {
+          console.log('サインアウト');
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     } catch (e) {
       if (e instanceof FirebaseError) {
-        console.log(e)
+        console.log(e);
       }
     }
-  }
+  };
 
   return (
     <header>
-        <div>
-            {userContext.state.id ?(<button onClick={handleSignOut}>
-             サインアウト
-         </button>):"ログアウト中"}
-        </div>
+      <div>
+        {userContext.state.id ? (
+          <button onClick={handleSignOut}>サインアウト</button>
+        ) : (
+          'ログアウト中'
+        )}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            navigate('/profile/update');
+          }}
+        >
+          プロフィール編集
+        </button>
+      </div>
+      <Outlet />
     </header>
- )
-}
+  );
+};

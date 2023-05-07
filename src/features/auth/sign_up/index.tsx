7d3c73e6f97a027d46a220b44704e0b21139cfd6
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-07 16:18:38 +0900

    update: sign_up

--- src/features/auth/sign_up/index.tsx
+++ src/features/auth/sign_up/index.tsx
@@ -1,38 +1,76 @@
-import { type FC, type FormEvent, useState } from 'react';
+import { type FC, type FormEvent, useState, useCallback, useContext } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { FirebaseError } from '@firebase/util';
-import {auth} from '../../../utils/firebase'
+import { auth, db,doc,setDoc } from '../../../utils/firebase';
+import { useValidation } from '../../../hooks/use_validation';
+import { ToastContext } from '../../../contexts/toast';
 
 const index: FC = () => {
+  const toast = useContext(ToastContext)
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
 
-  const handleChangeEmail: (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => void = (e) => {
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setEmail(e.currentTarget.value);
+    setEmailValidation(e.currentTarget.value);
   };
-  const handleChangePassword: (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => void = (e) => {
+  const handleChangePassword = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
     setPassword(e.currentTarget.value);
+    setPasswordValidation(e.currentTarget.value);
+  };
+  const handleChangeConfirmPassword = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setConfirmPassword(e.currentTarget.value);
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+  const [emailError, setEmailValidation] = useValidation(
+    email,
+    'メールアドレス',
+    'email'
+  );
+  const [passwordError, setPasswordValidation] = useValidation(
+    password,
+    'パスワード',
+    'password'
+  );
+
+  const signUp = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+    try{
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log(userCredential);
+        const cityRef = doc(db, 'profiles', userCredential.user.uid);
+        setDoc(cityRef,{
+          name:'',
+          icon: null,
+          birthOfDate:'',
+          gender:''
+        }).catch(()=>{
+          throw new Error
+        })
       })
-      .catch((e) => {
-        if (e instanceof FirebaseError) {
-          console.log(e);
-        }
+      .catch(() => {
+        throw new Error
       });
+    } catch(e){
+      toast.dispatch({
+        type: "SHOW_FAILED_TOAST",
+        payload:{
+          message: '処理に失敗しました'
+        }
+      })
+    }
   };
 
+  const checkValidParams = useCallback(() => {
+    return !emailError && !passwordError
+  }, [emailError, passwordError]);
+
   return (
-    <form className='form' onSubmit={handleSubmit}>
+    <form className='form' onSubmit={signUp}>
       <div className='form-body'>
         <div className='email'>
           <label className='form__label' htmlFor='email'>
@@ -46,6 +84,7 @@ const index: FC = () => {
             value={email}
             onChange={handleChangeEmail}
           />
+          <p>{emailError}</p>
         </div>
         <div className='password'>
           <label className='form__label' htmlFor='password'>
@@ -59,6 +98,7 @@ const index: FC = () => {
             value={password}
             onChange={handleChangePassword}
           />
+          <p>{passwordError}</p>
         </div>
         <div className='confirm-password'>
           <label className='form__label' htmlFor='confirmPassword'>
@@ -69,11 +109,14 @@ const index: FC = () => {
             type='password'
             id='confirmPassword'
             placeholder='Confirm Password'
+            value={confirmPassword}
+            onChange={handleChangeConfirmPassword}
           />
+          <p>{password !== confirmPassword && 'パスワードが一致しません'}</p>
         </div>
       </div>
       <div className='footer'>
-        <button type='submit' className='btn'>
+        <button type='submit' className='btn' disabled={!checkValidParams()}>
           Register
         </button>
       </div>

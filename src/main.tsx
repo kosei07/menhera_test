d728732142d20f2e35ecbb69255055e28950c1ce
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-07 16:17:43 +0900

    update: main.tsx for provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Router } from './Router.tsx';
 import { RouterProvider } from 'react-router-dom';
+import UserContextProvider from './contexts/user/index.tsx';
+import ToastContextProvider from './contexts/toast/index.tsx';
+import { Header } from './components/header/index.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <ToastContextProvider>
+      <UserContextProvider>
+        <Header/>
+        <RouterProvider router={Router} />
+      </UserContextProvider>
+    </ToastContextProvider>
   </React.StrictMode>
 );

c97fc3c77c18f82787eb5b698f39fcc392a96dea
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-07 18:18:38 +0900

    feat: VITE_APP_FIREBASE_STORAGE_BASE_URL

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
     readonly VITE_APP_MESSAGING_SENDER_ID: string,
     readonly VITE_APP_APP_ID: string,
     readonly VITE_APP_MEASUREMENT_ID: string
+    readonly VITE_APP_FIREBASE_STORAGE_BASE_URL: string
   }
   
   interface ImportMeta {

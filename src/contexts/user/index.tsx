e127606bf8a9f11d4397fdb2d40fdbe57b3474f8
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-08 12:13:36 +0900

    fix: userContext state type

--- src/contexts/user/index.tsx
+++ src/contexts/user/index.tsx
@@ -1,28 +1,32 @@
-import { createContext, useReducer,type FC,type ReactNode } from 'react'
-import { userReducer, initialState, type State, type Action } from '../../reducers/user/index'
-
+import { createContext, useReducer, type FC, type ReactNode } from 'react';
+import {
+  userReducer,
+  initialState,
+  type Action,
+} from '../../reducers/user/index';
+import { type USER_TYPE } from '../../type';
 export const contextValue: {
-  state: State
-  dispatch: React.Dispatch<Action>
+  state: USER_TYPE;
+  dispatch: React.Dispatch<Action>;
 } = {
   state: initialState,
   dispatch: () => null,
-}
+};
 
-export const UserContext = createContext(contextValue)
+export const UserContext = createContext(contextValue);
 
 interface Props {
-  children: ReactNode
+  children: ReactNode;
 }
 
 const UserContextProvider: FC<Props> = ({ children }) => {
-  const [state, dispatch] = useReducer(userReducer, initialState)
+  const [state, dispatch] = useReducer(userReducer, initialState);
 
   return (
     <UserContext.Provider value={{ state, dispatch }}>
       {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
 
-export default UserContextProvider
+export default UserContextProvider;

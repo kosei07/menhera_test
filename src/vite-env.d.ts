d4330626e4319cc2796698fbef722e1d46eb4001
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-08 09:35:33 +0900

    update: vite-env.d.ts for module css

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,16 @@
 interface ImportMetaEnv {
-    readonly VITE_APP_API_KEY: string,
-    readonly VITE_APP_AUTH_DOMAIN: string,
-    readonly VITE_APP_PROJECT_ID: string,
-    readonly VITE_APP_STORAGE_BUCKET: string,
-    readonly VITE_APP_MESSAGING_SENDER_ID: string,
-    readonly VITE_APP_APP_ID: string,
-    readonly VITE_APP_MEASUREMENT_ID: string
-    readonly VITE_APP_FIREBASE_STORAGE_BASE_URL: string
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
+  readonly VITE_APP_API_KEY: string;
+  readonly VITE_APP_AUTH_DOMAIN: string;
+  readonly VITE_APP_PROJECT_ID: string;
+  readonly VITE_APP_STORAGE_BUCKET: string;
+  readonly VITE_APP_MESSAGING_SENDER_ID: string;
+  readonly VITE_APP_APP_ID: string;
+  readonly VITE_APP_MEASUREMENT_ID: string;
+  readonly VITE_APP_FIREBASE_STORAGE_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '*.css';

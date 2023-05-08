427f4465a002e551372b2ba3e06ebf3023706cff
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-08 13:58:10 +0900

    update: input style

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,5 @@
 import { type FC } from 'react';
+import classes from './index.module.css';
 
 interface Props {
   label: string;
@@ -11,18 +12,16 @@ interface Props {
 
 const index: FC<Props> = (props) => {
   return (
-    <div className='input_wrapper'>
-      <label className='form__label' htmlFor='email'>
-        {props.label}
-      </label>
+    <div className={classes.input_wrapper}>
+      <label className={classes.label}>{props.label}</label>
       <input
         type={props.type}
-        className='input'
+        className={classes.input}
         placeholder={props.placeholder}
         value={props.value}
         onChange={props.onChangeHandler}
       />
-      <p> {props.valueError}</p>
+      <p className={classes.error_message}> {props.valueError}</p>
     </div>
   );
 };

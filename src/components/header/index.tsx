c0c2ede1d150473fe919885a939e75ecb2e2db8c
井上皓聖 <inouekousei@inoueakirashounoMacBook-Air.local>
2023-05-09 12:27:47 +0900

    update: header display

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import { type FC } from 'react';
 import { UserContext } from '../../contexts/user';
 import { useContext } from 'react';
 import { auth, signOut, FirebaseError } from '../../utils/firebase';
-import { useNavigate, Outlet } from 'react-router-dom';
+import { useNavigate, useLocation, Outlet } from 'react-router-dom';
 import classes from './index.module.css';
 
 interface Props {
@@ -11,6 +11,7 @@ interface Props {
 
 export const Header: FC<Props> = (props) => {
   const navigate = useNavigate();
+  const location = useLocation();
   const userContext = useContext(UserContext);
   const handleSignOut = (): void => {
     try {
@@ -32,57 +33,59 @@ export const Header: FC<Props> = (props) => {
     <header className={classes.header}>
       <div className={classes.header__wrapper}>
         <div className={classes.logo}>{props.title}</div>
-        <nav className={classes.nav}>
-          <button
-            className={classes.nav__toggle}
-            aria-expanded='false'
-            type='button'
-          >
-            menu
-          </button>
-          <ul className={classes.nav__wrapper}>
-            {userContext.state.id && (
-              <>
+        {!location.pathname.includes('auth') && (
+          <nav className={classes.nav}>
+            <button
+              className={classes.nav__toggle}
+              aria-expanded='false'
+              type='button'
+            >
+              menu
+            </button>
+            <ul className={classes.nav__wrapper}>
+              {userContext.state.id && (
                 <>
-                  {userContext.state.name && (
-                    <>
-                      <li className={classes.nav__item}>
-                        <div
-                          onClick={() => {
-                            navigate('/');
-                          }}
-                        >
-                          ホーム
-                        </div>
-                      </li>
-                      <li className={classes.nav__item}>
-                        <div
-                          onClick={() => {
-                            navigate('/profile/update');
-                          }}
-                        >
-                          プロフィール編集
-                        </div>
-                      </li>
-                      <li className={classes.nav__item}>
-                        <div
-                          onClick={() => {
-                            navigate('/book/create');
-                          }}
-                        >
-                          書籍レビュー作成
-                        </div>
-                      </li>
-                    </>
-                  )}
+                  <>
+                    {userContext.state.name && (
+                      <>
+                        <li className={classes.nav__item}>
+                          <div
+                            onClick={() => {
+                              navigate('/');
+                            }}
+                          >
+                            ホーム
+                          </div>
+                        </li>
+                        <li className={classes.nav__item}>
+                          <div
+                            onClick={() => {
+                              navigate('/profile/update');
+                            }}
+                          >
+                            プロフィール編集
+                          </div>
+                        </li>
+                        <li className={classes.nav__item}>
+                          <div
+                            onClick={() => {
+                              navigate('/book/create');
+                            }}
+                          >
+                            書籍レビュー作成
+                          </div>
+                        </li>
+                      </>
+                    )}
+                  </>
+                  <li className={classes.nav__item}>
+                    <div onClick={handleSignOut}>サインアウト</div>
+                  </li>
                 </>
-                <li className={classes.nav__item}>
-                  <div onClick={handleSignOut}>サインアウト</div>
-                </li>
-              </>
-            )}
-          </ul>
-        </nav>
+              )}
+            </ul>
+          </nav>
+        )}
       </div>
       <Outlet />
     </header>
